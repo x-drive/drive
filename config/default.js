f943495503f5e4ffe4095e7cf65b73bff005e512
Edwin <rongfan@hudongpai.com>
2018-03-13 21:00:15 +0800

    [config/default] 基本规则

--- config/default.js
+++ config/default.js
@@ -1,14 +1,24 @@
-module.exports = fis => {
-    fis.match("*.less", {
-        "parser": fis.plugin("less")
-        ,"rExt": ".css"
-    });
+fis.match("*.less", {
+    "parser": fis.plugin("less")
+    ,"rExt": ".css"
+});
 
-    fis.match("/{components,component_modules}/**.js", {
+fis.media("prod")
+    .match("/{components,component_modules}/**.js", {
         "optimizer": fis.plugin("uglify-js")
+    })
+    .match("*.{css,less}", {
+        "optimizer": fis.plugin("clean-css")
+    })
+    .match("*.png", {
+        "optimizer": fis.plugin("png-compressor")
     });
 
-    fis.match("/server/**.js", {
+var version = fis.config.get("version");
+var name = fis.config.get("name");
+var urlPrefix = fis.config.get("urlPrefix");
+
+fis.match("/server/**.js", {
         "optimizer": null
         ,"isMod": false
         ,"useCompile": false
@@ -18,17 +28,8 @@ module.exports = fis => {
         ,"isJsLike": false
         ,"isCssLike": false
         ,"useMap": false
-    });
-
-    fis.match("*.{css,less}", {
-        "optimizer": fis.plugin("clean-css")
-    });
-
-    fis.match("*.png", {
-        "optimizer": fis.plugin("png-compressor")
-    });
-
-    fis.match("/@server/(**)", {
+    })
+    .match("/@server/(**)", {
         "isMod": false
         ,"optimizer": null
         ,"useCompile": false
@@ -39,15 +40,30 @@ module.exports = fis => {
         ,"isCssLike": false
         ,"useMap": false
         ,"release": "/server/$1"
+    })
+    .match("/component_modules/{(*).js,**/(*).js}", {
+        "isMod": true
+        ,"moduleId": "$1"
+    })
+    .match("/component_modules/(**)", {
+        "release": "/public/c/$1"
+        ,"url": `${urlPrefix}/c/${name}/${version}/$1`
+    })
+    .match("/components/(**)", {
+        "release": `/public/c/${name}/${version}/$1`
+        ,"url": `${urlPrefix}/c/${name}/${version}/$1`
+    })
+    .match("/views/(**)", {
+        "release": `/public/${name}/${version}/$1`
+    })
+    .match("README.md", {
+        "release": false
     });
 
-    fis.config.set("settings.optimizer.uglify-js", {
-        "mangle": {
-            "except": "exports, module, require, define"
-        }
-    });
+fis.config.set("settings.optimizer.uglify-js", {
+    "mangle": {
+        "except": "exports, module, require, define"
+    }
+});
 
-    // fis.hook("module", {
-    //     "mode": "commonJs"
-    // });
-}
+fis.hook("commonjs");
