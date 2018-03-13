8d7652b38030c02d4982acd8e58e3fd42f6431b4
Edwin <rongfan@hudongpai.com>
2018-03-13 21:00:03 +0800

    [index] 异步加载配置

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ fis.require.prefixes = ["drive", "scrat", "fis3", "fis"];
 fis.cli.name = "drive";
 fis.cli.info = require("./package.json");
 fis.cli.help.commands = [ "release", "install", "server", "init"];
+fis.on("conf:loaded", function(){
+    require("./config/default");
+});
 
-require("./config/default")(fis);
+Object.defineProperty(global, "drive", {
+    "enumerable" : true
+    ,"writable" : false
+    ,"value" : fis
+});
