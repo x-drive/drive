7baffdc3e6a92bb84635bdf02150ea8990c227fd
Edwin <rongfan@hudongpai.com>
2018-05-18 15:25:41 +0800

    修改命令前缀

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fis = module.exports = require("fis3");
-fis.require.prefixes = ["drive", "scrat", "fis3", "fis"];
-fis.cli.name = "drive";
+fis.require.prefixes = ["dirve", "scrat", "fis3", "fis"];
+fis.cli.name = "dirve";
 fis.cli.info = require("./package.json");
 fis.cli.help.commands = [ "release", "install", "server", "init"];
 
-Object.defineProperty(global, "drive", {
+Object.defineProperty(global, "dirve", {
     "enumerable" : true
     ,"writable" : false
     ,"value" : fis
