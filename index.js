985fbe918b50d5e29c01f97241e3013bc9a5666c
Edwin <rongfan@hudongpai.com>
2018-06-07 11:24:04 +0800

    [index] 修改命令版本输出

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ fis.require.prefixes = ["dirve", "scrat", "fis3", "fis"];
 fis.cli.name = "dirve";
 fis.cli.info = require("./package.json");
 fis.cli.help.commands = [ "release", "install", "server", "init"];
+fis.cli.version = require("./version");
 
 Object.defineProperty(global, "dirve", {
     "enumerable" : true
